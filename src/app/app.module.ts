260e244667db7e1fce5025b7e44cc83c6229d9fd
Tzipora Gutmann <rgutmann4@student.touro.edu>
2022-11-22 19:42:42 -0500

    basic app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,51 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule, RouterOutlet, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatOptionModule } from '@angular/material/core';
+import { MatCommonModule } from '@angular/material/core';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { HttpClientModule } from  '@angular/common/http';
+import { WeatherReportComponent } from './weather-report/weather-report.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
 
+const routes: Routes = [
+  {
+    path: "",
+    component: WeatherReportComponent
+  },
+  {
+    path: ":locationName",
+    component: WeatherReportComponent
+  }
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherReportComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatNativeDateModule,
+    MatToolbarModule,
+    MatOptionModule,
+    MatCommonModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    RouterOutlet,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatCardModule,
+    RouterModule.forRoot(routes),
+    MatProgressBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
